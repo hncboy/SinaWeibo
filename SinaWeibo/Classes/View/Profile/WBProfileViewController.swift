15f903ce5ea923ebde2c27249bab03cc96c5e899
hncboy <619452863@qq.com>
2018-12-14 22:47:39 -0800

    文件结构部署，设置启动文件

---
+++ SinaWeibo/Classes/View/Profile/WBProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WBProfileViewController.swift
+//  SinaWeibo
+//
+//  Created by hncboy on 2018/12/14.
+//  Copyright © 2018年 hncboy. All rights reserved.
+//
+
+import UIKit
+
+class WBProfileViewController: WBBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

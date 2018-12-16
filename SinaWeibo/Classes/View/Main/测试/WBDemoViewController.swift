1587babadba9481758ceb9e087160e8928b845f6
hncboy <619452863@qq.com>
2018-12-16 01:11:21 -0800

    扩展测试控制器，一直push

--- SinaWeibo/Classes/View/Main/测试/WBDemoViewController.swift
+++ SinaWeibo/Classes/View/Main/测试/WBDemoViewController.swift
@@ -12,24 +12,25 @@ class WBDemoViewController: WBBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //设置标题
+        title = "第 \(navigationController?.childViewControllers.count ?? 0) 个"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - 监听方法
+    /// 继续PUSH一个新的控制器
+    func showNext() {
+        let vc = WBDemoViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
-    */
+}
 
+extension WBDemoViewController {
+    
+    /// 重写父类方法
+    override func setupUI() {
+        super.setupUI()
+        // 设置右侧的控制器
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "下一个", style: .plain, target: self, action: #selector(showNext))
+    }
 }

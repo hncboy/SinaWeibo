994daad9714234f1fb6548263ecbd6eeccc056ef
hncboy <619452863@qq.com>
2018-12-20 00:44:35 -0800

    在AppDelegate模拟加载网络应用程序配置json

--- SinaWeibo/AppDelegate.swift
+++ SinaWeibo/AppDelegate.swift
@@ -22,7 +22,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = WBMainViewController()
         window?.makeKeyAndVisible()
         
+        loadAppInfo()
+        
         return true
     }
 }
 
+// MARK: - 从服务器加载应用程序信息
+extension AppDelegate {
+    
+    func loadAppInfo() {
+        
+        
+        // 1.模拟异步
+        DispatchQueue.global().async {
+            
+            // 1>url
+            let url = Bundle.main.url(forResource: "main.json", withExtension: nil)
+            
+            // 2>data
+            let data = NSData(contentsOf: url!)
+            
+            // 3>写入磁盘
+            let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let jsonPath = (docDir as NSString).appendingPathComponent("main.json")
+            data?.write(toFile: jsonPath, atomically: true)
+            
+            print("应用程序加载完毕 \(jsonPath)")
+        }
+    }
+}

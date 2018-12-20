cb4252683a5409f0162431f919129f8e97568376
hncboy <619452863@qq.com>
2018-12-19 19:45:09 -0800

    懒加载访客视图控件，extension和懒加载类型的细节

--- SinaWeibo/Classes/View/Main/VisitorView/WBVisitorView.swift
+++ SinaWeibo/Classes/View/Main/VisitorView/WBVisitorView.swift
@@ -20,6 +20,37 @@ class WBVisitorView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - 私有控件
+    /// 懒加载属性只有调用UIKit控件的指定构造函数不需要使用类型
+    /// 图像视图
+    private lazy var iconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    
+    /// 小房子
+    private lazy var houseIconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+    
+    /// 提示标签
+    private lazy var tipLabel: UILabel = UILabel.cz_label(
+        withText: "先去关注一些人吧，再来看看有什么惊喜",
+        fontSize: 14,
+        color: UIColor.darkGray)
+    
+    /// 注册按钮
+    private lazy var registerButton: UIButton = UIButton.cz_textButton(
+        "注册",
+        fontSize: 16,
+        normalColor: UIColor.orange,
+        highlightedColor: UIColor.black,
+        backgroundImageName: "common_button_white_disable")
+    
+    /// 登录按钮
+    private lazy var loginButton: UIButton = UIButton.cz_textButton(
+        "登录",
+        fontSize: 16,
+        normalColor: UIColor.darkGray,
+        highlightedColor: UIColor.black,
+        backgroundImageName: "common_button_white_disable")
+    
 }
 
 // MARK: - 设置界面

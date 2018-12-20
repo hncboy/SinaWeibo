5bb6b875f343974a8fdf3192266e914a360064f1
hncboy <619452863@qq.com>
2018-12-19 19:59:46 -0800

    原生自动布局和小房子图像视图布局

--- SinaWeibo/Classes/View/Main/VisitorView/WBVisitorView.swift
+++ SinaWeibo/Classes/View/Main/VisitorView/WBVisitorView.swift
@@ -24,19 +24,19 @@ class WBVisitorView: UIView {
     // MARK: - 私有控件
     /// 懒加载属性只有调用UIKit控件的指定构造函数不需要使用类型
     /// 图像视图
-    private lazy var iconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    lazy var iconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
     
     /// 小房子
-    private lazy var houseIconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+    lazy var houseIconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
     
     /// 提示标签
-    private lazy var tipLabel: UILabel = UILabel.cz_label(
+    lazy var tipLabel: UILabel = UILabel.cz_label(
         withText: "先去关注一些人吧，再来看看有什么惊喜",
         fontSize: 14,
         color: UIColor.darkGray)
     
     /// 注册按钮
-    private lazy var registerButton: UIButton = UIButton.cz_textButton(
+    lazy var registerButton: UIButton = UIButton.cz_textButton(
         "注册",
         fontSize: 16,
         normalColor: UIColor.orange,
@@ -44,7 +44,7 @@ class WBVisitorView: UIView {
         backgroundImageName: "common_button_white_disable")
     
     /// 登录按钮
-    private lazy var loginButton: UIButton = UIButton.cz_textButton(
+    lazy var loginButton: UIButton = UIButton.cz_textButton(
         "登录",
         fontSize: 16,
         normalColor: UIColor.darkGray,
@@ -58,5 +58,49 @@ extension WBVisitorView {
     
     func setupUI() {
         backgroundColor = UIColor.white
+        
+        // 1.添加控件
+        addSubview(iconView)
+        addSubview(houseIconView)
+        addSubview(tipLabel)
+        addSubview(registerButton)
+        addSubview(loginButton)
+        
+        // 2.取消autoresizing
+        for v in subviews {
+            v.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        // 3.自动布局
+        // 1>图像识别
+        addConstraint(NSLayoutConstraint(item: iconView,
+                                         attribute: .centerX,
+                                         relatedBy: .equal,
+                                         toItem: self,
+                                         attribute: .centerX,
+                                         multiplier: 1.0,
+                                         constant: 0))
+        addConstraint(NSLayoutConstraint(item: iconView,
+                                         attribute: .centerY,
+                                         relatedBy: .equal,
+                                         toItem: self,
+                                         attribute: .centerY,
+                                         multiplier: 1.0,
+                                         constant: -60))
+        // 2>小房子
+        addConstraint(NSLayoutConstraint(item: houseIconView,
+                                         attribute: .centerX,
+                                         relatedBy: .equal,
+                                         toItem: iconView,
+                                         attribute: .centerX,
+                                         multiplier: 1.0,
+                                         constant: 0))
+        addConstraint(NSLayoutConstraint(item: houseIconView,
+                                         attribute: .centerY,
+                                         relatedBy: .equal,
+                                         toItem: iconView,
+                                         attribute: .centerY,
+                                         multiplier: 1.0,
+                                         constant: 0))
     }
 }

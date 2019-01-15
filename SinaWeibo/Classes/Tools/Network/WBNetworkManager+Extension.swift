b51574b3f1cf0dd188bd436ab3106cbf34a0ad7e
hncboy <619452863@qq.com>
2019-01-15 04:14:12 -0800

    下拉刷新微博数据

--- SinaWeibo/Classes/Tools/Network/WBNetworkManager+Extension.swift
+++ SinaWeibo/Classes/Tools/Network/WBNetworkManager+Extension.swift
@@ -13,13 +13,18 @@ extension WBNetworkManager {
     
     /// 加载微博数据字典数组
     ///
+    /// - parameter since_id: 返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
+    /// - parameter max_id: 返回ID小于或等于max_id的微博，默认为0。
     /// - parameter completion: 完成回调[List: 微博字典数组/是否成功]
-    func statusList(completion: @escaping (_ list: [[String: AnyObject]]?, _ isSuccess: Bool) -> ()) {
+    func statusList(since_id: Int64 = 0, max_id: Int64 = 0, completion: @escaping (_ list: [[String: AnyObject]]?, _ isSuccess: Bool) -> ()) {
         
         // 用网络工具加载微博数据
         let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
         
-        tokenRequest(URLString: urlString, parameters: nil) { (json, isSuccess) in
+        // Swift 中 Int 可以转换成 AnyObject 但是 Int64不行
+        let params = ["since_id": "\(since_id)", "max_id": "\(max_id)"]
+        
+        tokenRequest(URLString: urlString, parameters: params as [String : AnyObject]?) { (json, isSuccess) in
             
             // 从json中获取statuses字典数组
             // 如果as?失败，result=nil
--- SinaWeibo/Classes/ViewModel/WBStatusListViewModel.swift
+++ SinaWeibo/Classes/ViewModel/WBStatusListViewModel.swift
@@ -26,7 +26,11 @@ class WBStatusListViewModel {
     /// - parameter completion：完成回调
     func loadStatus(completion: @escaping (_ isSuccess: Bool) -> ()) {
         
-        WBNetworkManager.shared.statusList{ (list, isSuccess) in
+        // since_id 取出数组中第一条微博的id
+        let since_id = statusList.first?.id ?? 0
+        
+
+        WBNetworkManager.shared.statusList(since_id: since_id, max_id: 0) { (list, isSuccess) in
         
             // 1.字典转模型
             guard let array = NSArray.yy_modelArray(with: WBStatus.self, json: list ?? []) as? [WBStatus] else {
@@ -35,8 +39,11 @@ class WBStatusListViewModel {
                 return
             }
             
-            // 2.拼接数据
-            self.statusList += array
+            print("刷新到 \(array.count) 条数据")
+            
+            // 2.FIXME: 拼接数据
+            // 下拉刷新，应该将结果数组拼接在数组前面
+            self.statusList = array + self.statusList
             
             // 3.完成回调
             completion(isSuccess)

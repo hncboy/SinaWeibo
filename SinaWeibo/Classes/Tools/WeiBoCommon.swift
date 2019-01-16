4e5b05584f3d2c9b3b20ef454b6768146df366d8
hncboy <619452863@qq.com>
2019-01-16 15:57:01 -0800

    添加应用程序信息

--- SinaWeibo/Classes/Tools/WeiBoCommon.swift
+++ SinaWeibo/Classes/Tools/WeiBoCommon.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+// MARk: - 应用程序信息
+/// 应用程序 ID
+let WBAppKey = "1306087773"
+/// 应用程序加密信息(开发着可以申请修改)
+let WBAppSecret = "f45f3d4f16a0eea0d45b2d8a298dd2a3"
+/// 回调地址
+let WBRedirectURI = "http://baidu.com"
+
 // MARK: - 全局通知定义
 /// 用户需要登录通知
 let WBUserShouldLoginNotification = "WBUserShouldLoginNotification"

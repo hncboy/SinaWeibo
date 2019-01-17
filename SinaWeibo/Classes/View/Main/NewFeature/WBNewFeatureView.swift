781412663e90a52c7ec79929595cbf37d7bdd433
hncboy <619452863@qq.com>
2019-01-17 15:42:44 -0800

    准备欢迎视图和新特性视图

---
+++ SinaWeibo/Classes/View/Main/NewFeature/WBNewFeatureView.swift
@@ -0,0 +1,23 @@
+//
+//  WBNewFeatureView.swift
+//  SinaWeibo
+//
+//  Created by hncboy on 2019/1/17.
+//  Copyright © 2019年 hncboy. All rights reserved.
+//
+
+import UIKit
+
+/// 新特性视图
+class WBNewFeatureView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.orange
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ SinaWeibo/Classes/View/Main/NewFeature/WBWelcomeView.swift
@@ -0,0 +1,23 @@
+//
+//  WBWelcomeView.swift
+//  SinaWeibo
+//
+//  Created by hncboy on 2019/1/17.
+//  Copyright © 2019年 hncboy. All rights reserved.
+//
+
+import UIKit
+
+/// 欢迎视图
+class WBWelcomeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

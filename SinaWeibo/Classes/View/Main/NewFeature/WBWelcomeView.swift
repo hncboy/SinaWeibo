732df6e702e4770ec79940974d260aaf99b7b998
hncboy <619452863@qq.com>
2019-01-17 17:25:50 -0800

    欢迎界面自动布局动画实现

--- SinaWeibo/Classes/View/Main/NewFeature/WBWelcomeView.swift
+++ SinaWeibo/Classes/View/Main/NewFeature/WBWelcomeView.swift
@@ -10,6 +10,12 @@ import UIKit
 
 /// 欢迎视图
 class WBWelcomeView: UIView {
+    
+    
+    
+    @IBOutlet weak var bottomCons: NSLayoutConstraint!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var iconView: UIImageView!
 
     class func welcomeView() -> WBWelcomeView {
         
@@ -22,4 +28,33 @@ class WBWelcomeView: UIView {
         
         return v
     }
+    
+    /// 视图被添加到 window 上，表示视图已经显示
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        
+        // 视图是使用自动布局来设置的，只是设置了约束
+        // - 当视图被添加到窗口时，根据父视图的大小，计算约束值，更新控件位置
+        // - layoutIfNeeded 会直接按照当前的约束直接更新控件位置
+        // - 执行之后，控件所在位置，就是 XIB 中国布局的位置
+        self.layoutIfNeeded()
+        
+        bottomCons.constant = bounds.size.height - 200
+        
+        // 如果控件们的 frame 还没有计算好，所有的约束会一起动画
+        UIView.animate(
+            withDuration: 1.0,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 0,
+            options: [],
+            animations: {
+                
+                // 更新约束
+                self.layoutIfNeeded()
+        }) { (_) in
+            
+        }
+    }
 }
+
